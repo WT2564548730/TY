4f829a9f02bcdb960c40ff47d81784daaeb240fa
Tao2564548730 <2564548730@qq.com>
2019-06-13 11:54:21 +0800

    Initial Commit

---
+++ TY/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TY
+//
+//  Created by W20181105881 on 2019/6/13.
+//  Copyright © 2019 W20181105881. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
